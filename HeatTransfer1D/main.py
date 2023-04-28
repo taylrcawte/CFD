b11e392f088fbbc6ff8bb65d9b8f4996cf001790
Taylr Cawte <taylrcawte@gmail.com>
2023-04-28 09:50:43 -0400

    simulation is general with the case specifications included, may want to eliminate the ability to have multiple cases or figureout how to make configuring the script easier

--- HeatTransfer1D/main.py
+++ HeatTransfer1D/main.py
@@ -6,12 +6,12 @@ def main():
 
     sim = HeatTransfer1D(
         x_nodes=5, 
-        length=0.02, 
-        k=0.5, 
-        area=1, 
+        length=0.5, 
+        k=1000, 
+        area=10E-3, 
         bc1=100, 
-        bc2=200,
-        q=1000E3
+        bc2=500,
+        q=0
         )
     sim.calculate_coefficients()
     sim.solve() 
